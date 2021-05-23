f88edf5615944ccc19f1a2e6993e755833c64461
alejandromunozc <alejandromunozc@gmail.com>
2021-05-22 20:31:57 -0500

    BigCalendar y configuración

--- src/componets/calendar/CalendarScreen.jsx
+++ src/componets/calendar/CalendarScreen.jsx
@@ -1,10 +1,52 @@
-import React from 'react'
-import { NavBar } from '../ui/NavBar'
+import React from 'react';
+import {Calendar, momentLocalizer} from 'react-big-calendar';
+import moment from 'moment';
+import { NavBar } from '../ui/NavBar';
+import {messages} from '../../helpers/calendar-messages';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+import 'moment/locale/es'
+moment.locale('es');
+
+const localizer = momentLocalizer(moment);
+
+const events = [{
+  title: 'Cumple',
+  start: moment().toDate(),
+  end: moment().add(2, 'hours').toDate(),
+  bgcolor: '#fafafa'
+}];
 
 export const CalendarScreen = () => {
+
+  const eventStyleGetter = (event, start, end, isSelected) => {
+    console.log(event, start, end, isSelected);
+
+    const style = {
+      backgroundColor: '#367CF7',
+      borderRadius: '0px',
+      display: 'block',
+      opacity: 0.8,
+      color: 'white'
+    }
+
+    return {
+      style
+    }
+  }
+
   return (
-    <div>
+    <div className="calendar-screen">
       <NavBar />
+
+      <Calendar 
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        messages={messages}
+        eventPropGetter={eventStyleGetter}
+      />
     </div>
   )
 }
