04151d738c720cb160ee6d69a92e0d875f67a9c4
alejandromunozc <alejandromunozc@gmail.com>
2021-05-31 22:15:11 -0500

    agregando nuevo evento al reducer

--- src/componets/calendar/CalendarModal.jsx
+++ src/componets/calendar/CalendarModal.jsx
@@ -5,6 +5,7 @@ import moment from 'moment';
 import Swal from 'sweetalert2';
 import { useDispatch, useSelector } from 'react-redux';
 import { uiCloseModal } from '../../actions/ui';
+import { eventAddNew } from '../../actions/events';
 
 const customStyles = {
   content : {
@@ -82,6 +83,15 @@ export const CalendarModal = () => {
     if(title.trim().length < 2) {
       return setTitleValid(false);
     }
+
+    dispatch(eventAddNew({
+      ...formValues,
+      id: new Date().getTime(),
+      user: {
+        _id: 123,
+        name: 'Alejandro'
+      }
+    }));
 
     setTitleValid(true);
     closeModal();
